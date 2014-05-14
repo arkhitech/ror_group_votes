6106014fd7cd232c69756a281311973b999cfff5
Asim Mushtaq <asim.mushtaq@arkhitech.com>
2014-05-14 11:14:29 +0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GroupVoting::Application.config.secret_key_base = '782cc7fa5424c8dfb4af117d290d1c564dc363b4d43e3f9fc5d53edd2ebaf4b06c5d6d4ff33ae0655094fb1fc88f67d07f1a590f8802117de61947e92610256b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GroupVoting::Application.config.session_store :cookie_store, key: '_group_voting_session'
